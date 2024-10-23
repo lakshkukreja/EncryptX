a941d3078c9c7a4c8f5aca15a1eaae0e3ef3532a
lakshkukreja <lakshkukrejalk777@gmail.com>
2024-10-23 20:56:55 +0530

    Add files via upload

---
+++ CryptX/CryptX_customize.sh
@@ -0,0 +1,49 @@
+#!/sbin/sh
+# CryptX | @ | [0275] Commit: Please Modify Information Between " "; Only To Reflect It Properly In Main Flasher!
+
+# Display Project Name Details
+MOD_PROJECTNAME="FlymeOS";
+
+# Display Project Maintainer Name Details
+MOD_MAINTAINER="@test";
+
+# Display Project Version Details
+MOD_VERSION="V10.5.0.0A";
+
+# Display Project ReleaseCode Details
+MOD_RELEASECODE="Stable";
+
+# Display Project Supported Device Details
+MOD_DEVICENAME="POCO X5 Pro / Redmi Note 12 Pro SE";
+
+# Display Project Supported Model Details | Type "null"; for the fields wherein model name not required
+MOD_MODELNAME_01="redwood";
+MOD_MODELNAME_02="redwood_in";
+MOD_MODELNAME_03="redwood_cn";
+MOD_MODELNAME_04="redwood_global";
+MOD_MODELNAME_05="redwoodin";
+
+# Last Updated File On:
+ROM_MOD_DATE="17-06-2024";
+
+# Last Updated File At:
+ROM_MOD_TIME="11:49PM [IST]";
+
+# Display Security & Project Maintainer Name [LOCKED EDITING - NO CHANGES WILL REFLECTED IF YOU TRY TO MODIFY!]
+# CRYPTX_MAINTAINER="@AstroDEV63";
+
+# Display Security & Project Version [LOCKED EDITING - NO CHANGES WILL BE REFLECTED IF YOU TRY TO MODIFY!]
+# CRYPTX_VERSION="V1.0.2 [Plus]";
+
+# <-- Display A Prototype Of Flashing Output - NOTE: The Flashing Format Will Remain LOCKED! -->
+# ---------------------------------------
+# MOD_PROJECTNAME
+# ---------------------------------------
+# MOD_MAINTAINER
+# MOD_VERSION
+# MOD_RELEASECODE
+# MOD_DEVICENAME
+# MOD_DATE & MOD_TIME
+# ---------------------------------------
+# CRYPTX_VERSION BY CRYPTX_MAINTAINER
+# ---------------------------------------
