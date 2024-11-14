6835200ded9fabff2199d4b20894afa6bb5d4e3a
lakshkukreja <lakshkukrejalk777@gmail.com>
2024-11-15 00:15:15 +0530

    Update and rename CryptX_build-info.sh to EncryptX_build-info.sh

---
+++ EncryptX/EncryptX_build-info.sh
@@ -0,0 +1,71 @@
+#!/sbin/sh
+# Hey, Smartie
+# Enjoy Your Time Having A Look Below
+# Generating Information From Source Of EncryptX While Build Is Generating & Encrypting...
+
+# EncryptX Build Version
+EncryptX_Version="1.0.2";
+
+# EncryptX Build Variant
+EncryptX_Variant="[Plus]";
+
+# EncryptX Build Type
+EncryptX_Build_Type="Stable";
+
+# EncryptX NextGen SubModules List - Build Properties - [LOCKED EDITING]
+EncryptX_NextGen_SubModules_Supported_List() {
+# This Feature Checks Device Model To Proceed Flashing
+DLS_Device_Checker_Service="Yes";
+# This Feature Helps In Selecting Options Via Volume Key When Needed
+VKS_Volume_Key_Selector="No";
+# This Only Allows EncryptX Recoveries To Flash The .zip File
+CRS_EncryptX_Recovery_Support="No";
+# For Over Smart People Who Loves Exploring & Fiddling
+HBD_Hard_Brick_Device="Yes";
+# Disable EncryptX Log(s) Services
+DLS_Disable_Log_Service="No";
+# Shuffle Device Confidential Information In Log(s)
+USD_User_Shuffle_DeviceInformation="Yes";
+# Set An Definite Expiry Time Of Package, Which Gets Corrupt After 'X' Amount Of Time.
+CTS_EncryptX_TimeBomb_Service="Yes";
+# Add Data Dictonary Services Specially To Make Logs Even More Better And Encrypted In "update-binary" File.
+DDS_Data_Dictonary_Service="Yes";
+# Warning Alert Service(s)
+WAS_Warning_Alert_Service="Yes";
+}
+
+# EncryptX_Module_Package_Max_Support_ID="01";
+EncryptX_tools_support="busybox,fec,httools_static,lptools_static,magiskboot,magiskpolicy,snapshotupdater_static,zstd";
+EncryptX_firmware_directory="pedriot_firmware/firmware-update";
+EncryptX_firmware_support="changelog.txt,customize.sh,LICENSE,README.md";
+EncryptX_Fidelity_Information_01="17";
+EncryptX_Fidelity_Information_02="28";
+# @console: large file detected, dividing information into multiple split packages...
+EncryptX_Fidelity_Information_03="06";
+EncryptX_Fidelity_Information_04="48";
+# @console: compressing firmware-update folder files for a lesser size of build package...
+EncryptX_Fidelity_Information_05="20";
+EncryptX_Fidelity_Information_06="39";
+# @console: searching for index based information from github server...
+EncryptX_Fidelity_Information_07="24";
+EncryptX_Fidelity_Information_08="12";
+# @console: encrypting symetrically files -s -v -c -ls "CrpytX-ForceEncrypt.x", "EncryptX-SourceCode-Binary.sh", "update-binary"
+EncryptX_Fidelity_Information_09="23";
+EncryptX_Fidelity_Information_10="56";
+EncryptX_Fidelity_Information_11="59";
+EncryptX_Fidelity_Information_12="77";
+EncryptX_Fidelity_Information_13="59";
+EncryptX_Fidelity_Information_14="89";
+# @console: parsing the whole process into dumping function - "EncryptX_Fidelity_Information"
+PACKAGE_VARIABLES_Package_Flasher="Universal Recovery Supported";
+PACKAGE_VARIABLES_Device_OEM="Xiaomi";
+#
+# FILE_LOGS=Proceeding Flashing...
+# FILE_LOGS=Decrypting Files...
+# FILE_LOGS=Unzipping Images...
+#
+# BLANK_PLACEHOLDER=
+#
+#
+# Thanks For Wasting Your Time Here! xD
+# @console: build is ready!
